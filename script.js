a82241b895c7817c03482f3683dff60685f63114
João Paulo <joao.filho488@aluno.ce.gov.br>
2025-08-21 14:26:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let nome = prompt("Digite seu nome: ")
+const nome = prompt("Digite seu nome: ")
 let idade = parseInt(prompt("Digite sua idade: "))
-let senha = prompt("Digite sua senha: ")
+const senha = prompt("Digite sua senha: ")
 //inicio do cadastro
 
 let senhaVerificada
@@ -8,12 +8,16 @@ let nomeVerificada
 //variaveis para identificar 
 
 if (idade >=13) {
-    alert("Parabéns você foi cadastrado. Vamos fazer o login")
+    alert(`
+        --- Cadastro de Usuário ---
+        Digite seu nome: ${nome}
+        Digite sua idade: ${idade}
+        Digite uma senha: ${senha}
+        `)
     nomeVerificada = prompt("Digite seu nome: ")
     senhaVerificada = prompt("Digite sua senha: ")
     if (nomeVerificada == nome && senhaVerificada === senha)
         // faz o login do usuário
-
         {
         alert(`
             --- Login ---
@@ -23,10 +27,15 @@ if (idade >=13) {
             Login realizado com sucesso! Bem-vindo, ${nome}
             `)
     } //mostra que o usuário consegiu fazer o login
-
     else {alert("Nome ou senha incorretos. Tente novamente.")}
 } //mostra que o usuário errou o nome ou a senha
-
 else {
-    alert("Não foi possível se cadastrar. Idade mínima: 13 anos")
+    alert(`
+        --- Cadastro de Usuário ---
+        Digite seu nome: ${nome}
+        Digite sua idade: ${idade}
+        Digite uma senha: ${senha}
+
+        Você não pode se cadastrar. Idade mínima: 13 anos.
+`)
 }//diz ao usuário que não é possível fazer o cadastro   
